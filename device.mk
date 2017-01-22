2b16fc7fa6e8fd53a7fa3f23b8753b22e57883cd
ProfessorWoland <elsonebrate@gmail.com>
2017-01-22 22:12:29 +0100

    Update device.mk
    remove light sensoz coz it doesn't exist on device, fix typo on ht120

--- device.mk
+++ device.mk
@@ -35,7 +35,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
-#    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
     frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
     $(LOCAL_PATH)/configs/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
@@ -48,7 +47,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal.conf:system/etc/.tp/thermal.conf \
     $(LOCAL_PATH)/configs/thermal.off.conf:system/etc/.tp/thermal.off.conf \
-    $(LOCAL_PATH)/configs/ht120.mtc:system/etc/.tp/.htc120.mtc
+    $(LOCAL_PATH)/configs/ht120.mtc:system/etc/.tp/.ht120.mtc
 
 # System Properties
 #PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
